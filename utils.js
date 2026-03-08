864c9b9bd8c658884b136db20137c6cb69e754d7
Gloosi <glorsii56@outlook.com>
2026-03-09 02:36:06 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T23:35:59.793Z
-Commit: Fix broken links in documentation
+Timestamp: 2026-03-08T23:36:06.743Z
+Commit: Add export functionality for reports
