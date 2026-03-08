bbee08e8c8698d0208bf349ffd66dbb2f6b2e995
Gloosi <glorsii56@outlook.com>
2026-03-09 02:33:52 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T23:33:35.499Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2026-03-08T23:33:52.433Z
+Commit: Fix broken links in documentation
