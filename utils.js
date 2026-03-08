7a9a7b16dd7e6d352a5f81d41bd710940aa325f9
Gloosi <glorsii56@outlook.com>
2026-03-09 02:33:32 +0300

    Fix memory leak in event handlers

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T23:33:11.357Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-03-08T23:33:32.930Z
+Commit: Fix memory leak in event handlers
