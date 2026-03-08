0a37c17c5bc4346a374830b904132cf69b503f55
Gloosi <glorsii56@outlook.com>
2026-03-09 02:36:36 +0300

    Fix broken links in documentation

--- app.js
+++ app.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-08T23:36:24.943Z
-Commit: Patch security vulnerability in authentication
+Timestamp: 2026-03-08T23:36:37.034Z
+Commit: Fix broken links in documentation
